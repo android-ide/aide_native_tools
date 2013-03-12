30656f1f51f4bb747c77ec5ab553ad95cb3760a0
Hans Kratz <hans@appfour.com>
2013-03-12 17:01:19 +0100

    build files for aide native tools (work in progress)

---
+++ jni/Android.mk
@@ -0,0 +1,11 @@
+myPATH:= $(call my-dir)
+
+# Include makefiles here.
+#include $(myPATH)/external/expat/Android.mk
+#include $(myPATH)/external/libpng/Android.mk
+#include $(myPATH)/system/core/liblog/Android.mk
+#include $(myPATH)/system/core/libcutils/Android.mk
+#include $(myPATH)/build/libs/host/Android.mk
+#include $(myPATH)/frameworks/base/libs/utils/Android.mk
+include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
+#include $(myPATH)/frameworks/base/tools/aidl/Android.mk
---
+++ jni/Application.mk
@@ -0,0 +1,7 @@
+# uncomment following line in order to explicitly build static libs
+#APP_MODULES := libpng expat_static liblog libcutils libutils aapt aidl
+
+APP_STL := gnustl_static
+APP_PLATFORM := android-9
+APP_ABI := armeabi x86 mips
+
