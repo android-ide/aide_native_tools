2542cdf2245d0ffbd774a9912d6f5ce801f1bc6b
Hans Kratz <hans@appfour.com>
2013-03-13 08:06:16 +0100

    build liblog, libcutils

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,8 +3,8 @@ myPATH:= $(call my-dir)
 # Include makefiles here.
 include $(myPATH)/platform/external/expat/Android.mk
 include $(myPATH)/platform/external/libpng/Android.mk
-#include $(myPATH)/system/core/liblog/Android.mk
-#include $(myPATH)/system/core/libcutils/Android.mk
+include $(myPATH)/platform/system/core/liblog/Android.mk
+include $(myPATH)/platform/system/core/libcutils/Android.mk
 #include $(myPATH)/build/libs/host/Android.mk
 #include $(myPATH)/frameworks/base/libs/utils/Android.mk
 include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
