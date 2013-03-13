f85f2d8f60f1dce631f7faf11801da99705854e0
Hans Kratz <hans@appfour.com>
2013-03-13 07:51:59 +0100

    libpng build

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,7 @@ myPATH:= $(call my-dir)
 
 # Include makefiles here.
 include $(myPATH)/platform/external/expat/Android.mk
-#include $(myPATH)/external/libpng/Android.mk
+include $(myPATH)/platform/external/libpng/Android.mk
 #include $(myPATH)/system/core/liblog/Android.mk
 #include $(myPATH)/system/core/libcutils/Android.mk
 #include $(myPATH)/build/libs/host/Android.mk
