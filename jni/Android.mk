b372b18468e7849a19be80e15b46e5165e4c6a18
Hans Kratz <hans@appfour.com>
2013-03-13 08:32:37 +0100

    build libhost.a

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,7 +5,7 @@ include $(myPATH)/platform/external/expat/Android.mk
 include $(myPATH)/platform/external/libpng/Android.mk
 include $(myPATH)/platform/system/core/liblog/Android.mk
 include $(myPATH)/platform/system/core/libcutils/Android.mk
-#include $(myPATH)/build/libs/host/Android.mk
+include $(myPATH)/platform/build/libs/host/Android.mk
 include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
 include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
 #include $(myPATH)/frameworks/base/tools/aidl/Android.mk
