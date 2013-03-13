f18e5b09b3ecfb6d0f4ea29d401e5e7536445dbe
Hans Kratz <hans@appfour.com>
2013-03-13 08:25:02 +0100

    build libandroidfw

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,6 +6,6 @@ include $(myPATH)/platform/external/libpng/Android.mk
 include $(myPATH)/platform/system/core/liblog/Android.mk
 include $(myPATH)/platform/system/core/libcutils/Android.mk
 #include $(myPATH)/build/libs/host/Android.mk
-#include $(myPATH)/frameworks/base/libs/utils/Android.mk
+include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
 include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
 #include $(myPATH)/frameworks/base/tools/aidl/Android.mk
