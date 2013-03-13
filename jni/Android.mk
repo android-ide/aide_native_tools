a4a42a5e94cf752319674ac86f68ac765e28179f
Hans Kratz <hans@appfour.com>
2013-03-13 08:47:25 +0100

    build libutils

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,5 +7,6 @@ include $(myPATH)/platform/system/core/liblog/Android.mk
 include $(myPATH)/platform/system/core/libcutils/Android.mk
 include $(myPATH)/platform/build/libs/host/Android.mk
 include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
+include $(myPATH)/platform/frameworks/native/libs/utils/Android.mk
 include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
 #include $(myPATH)/frameworks/base/tools/aidl/Android.mk
