6b97db2ecf82a6cb8dd6f81c94cb7c31e0e37cad
Hans Kratz <hans@appfour.com>
2014-06-28 12:22:40 +0000

    Build position-independant executables (PIE) for Android L

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,8 +1,4 @@
-# uncomment following line in order to explicitly build static libs
-#APP_MODULES := libpng expat_static liblog libcutils libutils aapt aidl
-
-APP_STL := gnustl_static
+APP_PIE := true
 APP_PLATFORM := android-9
-APP_ABI := armeabi armeabi-v7a x86
-
-APP_OPTIM := release
+APP_ABI := armeabi-v7a
+APP_STL := gnustl_static
