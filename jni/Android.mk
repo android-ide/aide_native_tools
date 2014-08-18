a411029846b298180d7e769f0b05336edce1f252
Hans Kratz <kratz@omnicore.com>
2014-08-18 15:31:40 +0200

    l-preview updates

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,6 +7,7 @@ include $(myPATH)/platform/system/core/liblog/Android.mk
 include $(myPATH)/platform/system/core/libcutils/Android.mk
 include $(myPATH)/platform/build/libs/host/Android.mk
 include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
-include $(myPATH)/platform/frameworks/native/libs/utils/Android.mk
+include $(myPATH)/platform/system/core/libutils/Android.mk
 include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
 include $(myPATH)/platform/frameworks/base/tools/aidl/Android.mk
+include $(myPATH)/platform/system/core/libziparchive/Android.mk
--- jni/Application.mk
+++ jni/Application.mk
@@ -2,7 +2,17 @@
 #APP_MODULES := libpng expat_static liblog libcutils libutils aapt aidl
 
 APP_STL := gnustl_static
-APP_PLATFORM := android-9
+APP_PLATFORM := android-14
 APP_ABI := armeabi armeabi-v7a x86
+#APP_ABI := armeabi-v7a # for testing only
+APP_PIE := true
 
+#APP_OPTIM := debug
 APP_OPTIM := release
+
+AIDE_BUILD := 1
+
+APP_CFLAGS += -DAIDE_BUILD
+
+APP_CFLAGS += -DHAVE_ANDROID_OS -DHAVE_SYS_UIO_H -DHAVE_PTHREADS -DHAVE_SCHED_H -DHAVE_IOCTL -DHAVE_TM_GMTOF -DHAVE_SYS_SOCKET_H -DHAVE_ENDIAN_H -DHAVE_POSIX_FILEMAP -DHAVE_OFF64_T -DHAVE_EXPAT_CONFIG_H -D__STDC_FORMAT_MACROS -Ddprintf=fdprintf -DFAKE_LOG_DEVICE=1 -O0
+APP_CFLAGS += -DOS_PATH_SEPARATOR=\'/\'
