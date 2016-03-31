caeab67fc0159d695301e838885d237971edef3d
Hans Kratz <hans@appfour.com>
2016-03-31 17:19:51 +0200

    rebase on android_n_preview_1 (WIP)

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,9 +5,9 @@ include $(myPATH)/platform/external/expat/Android.mk
 include $(myPATH)/platform/external/libpng/Android.mk
 include $(myPATH)/platform/system/core/liblog/Android.mk
 include $(myPATH)/platform/system/core/libcutils/Android.mk
-include $(myPATH)/platform/build/libs/host/Android.mk
-include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
-include $(myPATH)/platform/system/core/libutils/Android.mk
-include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
-include $(myPATH)/platform/frameworks/base/tools/aidl/Android.mk
-include $(myPATH)/platform/system/core/libziparchive/Android.mk
+#include $(myPATH)/platform/build/libs/host/Android.mk
+#include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
+#include $(myPATH)/platform/system/core/libutils/Android.mk
+#include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
+#include $(myPATH)/platform/frameworks/base/tools/aidl/Android.mk
+#include $(myPATH)/platform/system/core/libziparchive/Android.mk
