e5c66bf9a87ea3f43fb6ad5015dc461da50e0844
Hans Kratz <hans@appfour.com>
2016-04-06 12:22:46 +0200

    updated build files: use clang, build all necessary modules

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,11 +3,11 @@ myPATH:= $(call my-dir)
 # Include makefiles here.
 include $(myPATH)/platform/external/expat/Android.mk
 include $(myPATH)/platform/external/libpng/Android.mk
-include $(myPATH)/platform/system/core/liblog/Android.mk
 include $(myPATH)/platform/system/core/libcutils/Android.mk
-#include $(myPATH)/platform/build/libs/host/Android.mk
-#include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
-#include $(myPATH)/platform/system/core/libutils/Android.mk
-#include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
+include $(myPATH)/platform/build/libs/host/Android.mk
+include $(myPATH)/platform/frameworks/base/libs/androidfw/Android.mk
+include $(myPATH)/platform/system/core/libutils/Android.mk
+include $(myPATH)/platform/system/core/base/Android.mk
+include $(myPATH)/platform/frameworks/base/tools/aapt/Android.mk
 #include $(myPATH)/platform/frameworks/base/tools/aidl/Android.mk
-#include $(myPATH)/platform/system/core/libziparchive/Android.mk
+include $(myPATH)/platform/system/core/libziparchive/Android.mk
